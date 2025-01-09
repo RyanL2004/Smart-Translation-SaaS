ec66b76bfe976def4de89855230cb3747c59ca23
RyanL2004 <rayanlouahche2004@gmail.com>
2025-01-09 17:07:07 +0000

    Add Sign up route to get Started button

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -2,22 +2,30 @@ import React from 'react'
 import './header.css'
 import people from '../../assets/people.png'
 import ai from '../../assets/ai.png'
+import { Link, useNavigate, navigate } from 'react-router-dom'
+import Signup from '../../components/Signup'
 
 const Header = () => {
+  const navigate = useNavigate();
+  const navigateToSignUp = () => {
+    navigate('/signup');
+  };
   return (
     <div className="gpt3__header section__padding" id="home"> 
       <div className="gpt3__header-content">
-        <h1 className="gradient__text">Let’s Build Something amazing with GPT-3 OpenAI</h1>
+        <h1 className="gradient__text">Let’s Build Something amazing with GPT-4</h1>
         <p> Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
       
         <div className="gpt3__header-content__input">
           <input type="email" placeholder="Your Email Address"></input>
-          <button type="button">Get Started</button>
+          <Link to="/signup">
+          <button type="button" onClick={navigateToSignUp}>Get Started</button>
+          </Link>
         </div>
 
         <div className="gpt3__header-content__people">
           <img src={people} alt="people" />
-          <p>1,600 people requested access a visit in last 24 hours</p>
+          <p>Over 1000 people requested access a visit in last 2 weeks</p>
         </div>
       </div>
       <div className="gpt3__header-image">
